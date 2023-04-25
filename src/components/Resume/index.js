3b01db677817758fd6505daba53053e5e9009ef2
omen <kybar089@gmail.com>
2023-04-24 23:06:58 -0500

    updated link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import resumePdf from "../assets/Resume-Kyle.pdf"
 
 function Resume() {
   return (
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href={resumePdf}>resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
